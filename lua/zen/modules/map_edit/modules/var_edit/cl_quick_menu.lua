eab3979f3d935541afc14c7e62e27a297365a8bf
jtaylerj <jtaylerj@gmail.com>
2022-09-03 20:01:29 +0100

    update quick menu

--- lua/zen/modules/map_edit/modules/var_edit/cl_quick_menu.lua
+++ lua/zen/modules/map_edit/modules/var_edit/cl_quick_menu.lua
@@ -1,38 +1,127 @@
 zen.nvars = zen.nvars or {}
 local nvars = zen.nvars
-
-local draw_NoTexture = draw.NoTexture
-
 local draw = zen.Import("ui.draw")
 
-local lp = LocalPlayer
 local cos = math.cos
 local sin = math.sin
 local pi = math.pi
-local blur = Material("pp/blurscreen")
-local tex = surface.GetTextureID("VGUI/white.vmt")
+local blur = Material("pp/sub")
 
 nvars.radial_menu = {}
 nvars.radial_menu.is_opened = false
 
-local founded_entity
 local _, fontHeight = ui.GetTextSize("W", 11)
 
 
 nvars.mt_EntityButtons = {}
 
-local prevSelected, prevSelectedVertex
-local function getSelected()
-	local mx, my = gui.MousePos()
-	local sw, sh = ScrW(), ScrH()
-	local total = #nvars.mt_EntityButtons
-	local w = math.min(sw * 0.45, sh * 0.45)
-	local sx, sy = sw / 2, sh / 2
-	local x2, y2 = mx - sx, my - sy
+local clr_circle = Color(100,100,100,150)
+local clr_active = Color(255,100,100,100)
+local clr_selected = Color(125, 125, 255, 100)
+local clr_selected_red = Color(255, 125, 125, 150)
+
+local sw, sh = ScrW(), ScrH()
+nvars.circles = {}
+nvars.circles[1] ={
+	type = "nvars",
+	x = sw*0.1,
+	y = sh*0.60,
+	max_r = math.min(sw*0.1, sh*0.1)
+}
+nvars.circles[2] ={
+	type = "ent_command",
+	x = sw*0.1,
+	y = sh*0.85,
+	color = Color(125, 100, 100, 100),
+	max_r = math.min(sw*0.1, sh*0.1),
+	content = {
+		{
+			text = "Remove",
+			color = COLOR.RED,
+			hover_color = clr_selected_red,
+			value = "remove"
+		},
+		{
+			text = "+Freeze",
+			color = color_white,
+			value = "freeze"
+		},
+		{
+			text = "-Freeze",
+			color = color_white,
+			value = "unfreeze"
+		},
+		{
+			text = "-Motion",
+			color = color_white,
+			value = "motion_disable"
+		},
+		{
+			text = "+Motion",
+			color = color_white,
+			value = "motion_enable"
+		},
+	},
+}
+nvars.circles[3] ={
+	type = "edit_vars",
+	x = sw*0.9,
+	y = sh*0.85,
+	text = "Change",
+	color = Color(100, 100, 100, 100),
+	max_r = math.min(sw*0.13, sh*0.13),
+	content = {
+		{
+			text = "Origin",
+			value = "change_pos"
+		},
+		{
+			text = "Angle",
+			value = "change_ang"
+		},
+		{
+			text = "Velocity",
+			value = "change_velocity"
+		},
+	},
+}
+nvars.circles[4] ={
+	type = "edit_modes",
+	x = sw*0.9,
+	y = sh*0.55,
+	text = "Change",
+	color = Color(100, 100, 100, 100),
+	max_r = math.min(sw*0.13, sh*0.13),
+	content = {
+		{
+			text = "Physics",
+			value = "change_pos"
+		},
+		{
+			text = "Bones",
+			value = "change_ang"
+		},
+		{
+			text = "Variables",
+			value = "change_velocity"
+		},
+	},
+}
+
+local function cursorInCircle(x, y, r)
+	local mx, my = input.GetCursorPos()
+
+	return math.Distance(x, y, mx, my) < r
+end
+
+local function getCircleSelected(x, y, r, sep)
+	local mx, my = input.GetCursorPos()
+	local x2, y2 = mx - x, my - y
+
 	local ang = 0
 	local dis = math.sqrt(x2 ^ 2 + y2 ^ 2)
 
-	if dis / w <= 1 then
+	if dis / r <= 1 then
 		if y2 <= 0 and x2 <= 0 then
 			ang = math.acos(x2 / dis)
 		elseif x2 > 0 and y2 <= 0 then
@@ -43,188 +132,168 @@ local function getSelected()
 			ang = pi * 2 - math.acos(x2 / dis)
 		end
 
-		return math.floor((1 - (ang - pi / 2 - pi / total) / (pi * 2) % 1) * total) + 1
+		return math.floor((1 - (ang - pi / 2 - pi / sep) / (pi * 2) % 1) * sep) + 1
 	end
+	return -1
 end
 
-local r = 0
+local function getCircleItemPos(sx, sy, r, sep, item_id)
+	local x, y = cos((item_id - 1) / sep * pi * 2 + pi * 1.5), sin((item_id - 1) / sep * pi * 2 + pi * 1.5)
+
+	return sx + r * 0.6 * x, sy + r * 0.6 * y - fontHeight / 2
+end
+
+local function getCircleDrawPolySelected(sx, sy, r, sep, item_id, saveTbl)
+	local add = pi * 1.5 + pi / sep
+	local add2 = pi * 1.5 - pi / sep
+
+	local lx, ly = cos((item_id - 1) / sep * pi * 2 + add), sin((item_id - 1) / sep * pi * 2 + add)
+
+	local vertexes = saveTbl.prevSelectedVertex
+
+	if saveTbl.prevSelected ~= item_id then
+		saveTbl.prevSelected = item_id
+		vertexes = {}
+		saveTbl.prevSelectedVertex = vertexes
+		local lx2, ly2 = cos((item_id - 1) / sep * pi * 2 + add2), sin((item_id - 1) / sep * pi * 2 + add2)
+
+		table.insert(vertexes, {
+			x = sx,
+			y = sy
+		})
+
+		table.insert(vertexes, {
+			x = sx + r * 1 * lx2,
+			y = sy + r * 1 * ly2
+		})
+
+		local max = math.floor(50 / sep)
+		for i = 0, max do
+			local addv = (add - add2) * i / max + add2
+			local vx, vy = cos((item_id - 1) / sep * pi * 2 + addv), sin((item_id - 1) / sep * pi * 2 + addv)
+
+			table.insert(vertexes, {
+				x = sx + r * 1 * vx,
+				y = sy + r * 1 * vy
+			})
+		end
+
+		table.insert(vertexes, {
+			x = sx + r * 1 * lx,
+			y = sy + r * 1 * ly
+		})
+	end
+
+	return vertexes
+end
+
+
 hook.Add("zen.map_edit.Render", "quickmenu", function(rendermode, priority, vw)
     if rendermode != RENDER_2D or priority != RENDER_POST then return end
     if not nvars.radial_menu.is_opened then return end
 
-	local ent = founded_entity
+	for id, v in pairs(nvars.circles) do
+		local x, y, r = v.x, v.y, v.max_r
+		local tContent = v.content
 
-	local sw, sh = ScrW(), ScrH()
-	local total = #nvars.mt_EntityButtons
+		if not tContent then
 
-	local w = math.min(sw * 0.4, sh * 0.4)
-	local h = w
-	r = Lerp(0.1, r, w)
-	local sx, sy = sw / 2, sh / 2
-	local selected = getSelected() or -1
-	local circleVertex = {}
-
-	local max = 50
-
-	if total == 0 then
-		r = 50
-	end
-
-	for i = 0, max do
-		local vx, vy = cos((pi * 2) * i / max), sin((pi * 2) * i / max)
-
-		table.insert(circleVertex, {
-			x = sx + r * 1 * vx,
-			y = sy + r * 1 * vy
-		})
-	end
-
-	render.ClearStencil()
-	render.SetStencilEnable(true)
-	render.SetStencilWriteMask(255)
-	render.SetStencilTestMask(255)
-	render.SetStencilReferenceValue(1)
-	render.SetStencilCompareFunction(STENCIL_ALWAYS)
-	render.SetStencilPassOperation(STENCIL_REPLACE)
-	render.SetStencilFailOperation(STENCIL_KEEP)
-	render.SetStencilZFailOperation(STENCIL_KEEP)
-	surface.SetDrawColor(0, 0, 0, 1)
-	surface.SetTexture(tex)
-	surface.DrawPoly(circleVertex)
-	render.SetStencilCompareFunction(STENCIL_EQUAL)
-	render.SetStencilPassOperation(STENCIL_KEEP)
-	render.SetMaterial(blur)
-	local steps = 3
-	local multiplier = 6
-
-	for i = 1, steps do
-		blur:SetFloat("$blur", (i / steps) * (multiplier or 6))
-		blur:Recompute()
-		render.UpdateScreenEffectTexture()
-		render.DrawScreenQuad()
-	end
-
-	if total == 0 then
-		surface.SetDrawColor(20, 20, 20, 25)
-	else
-		surface.SetDrawColor(20, 20, 20, 180)
-	end
-	draw_NoTexture()
-	surface.DrawRect(0, 0, ScrW(), ScrH())
-	render.SetStencilEnable(false)
-
-	local add = pi * 1.5 + pi / total
-	local add2 = pi * 1.5 - pi / total
-
-
-	for k, v in pairs(nvars.mt_EntityButtons) do
-		local can_use = true
-
-		local x, y = cos((k - 1) / total * pi * 2 + pi * 1.5), sin((k - 1) / total * pi * 2 + pi * 1.5)
-		local lx, ly = cos((k - 1) / total * pi * 2 + add), sin((k - 1) / total * pi * 2 + add)
-		local textCol = Color(255, 255, 255)
-
-		if not can_use then
-			textCol = Color(100, 100, 100)
+			continue
 		end
 
-		if selected == k then
-			local vertexes = prevSelectedVertex
+		local total = #tContent
 
-			if prevSelected ~= selected then
-				prevSelected = selected
-				vertexes = {}
-				prevSelectedVertex = vertexes
-				local lx2, ly2 = cos((k - 1) / total * pi * 2 + add2), sin((k - 1) / total * pi * 2 + add2)
+		draw.Circle(x, y, r, 50, v.color or clr_circle)
 
-				table.insert(vertexes, {
-					x = sx,
-					y = sy
-				})
+		local selID = getCircleSelected(x, y, r, total)
 
-				table.insert(vertexes, {
-					x = sx + w * 1 * lx2,
-					y = sy + h * 1 * ly2
-				})
 
-				local max = math.floor(50 / total)
-				for i = 0, max do
-					local addv = (add - add2) * i / max + add2
-					local vx, vy = cos((k - 1) / total * pi * 2 + addv), sin((k - 1) / total * pi * 2 + addv)
+		if selID > 0 then
+			local dat = tContent[selID]
+			local poly = getCircleDrawPolySelected(x, y, r, total, selID, v)
+			v.SelectID = selID
+			v.SelectValue = dat.value
 
-					table.insert(vertexes, {
-						x = sx + w * 1 * vx,
-						y = sy + h * 1 * vy
-					})
-				end
+			local clr = dat.hover_color or clr_selected
 
-				table.insert(vertexes, {
-					x = sx + w * 1 * lx,
-					y = sy + h * 1 * ly
-				})
-			end
-
-			if can_use then
-				surface.SetTexture(tex)
-				surface.SetDrawColor(125, 125, 255, 100)
-				surface.DrawPoly(vertexes)
-			end
+			draw.Circle(x, y, 20, 50, clr)
+			draw.DrawPoly(poly, clr)
+		else
+			v.SelectID = nil
+			v.SelectValue = nil
 		end
 
-		draw.Text(v.string, 13, sx + r * 0.6 * x, sy + r * 0.6 * y - fontHeight / 2, textCol, 1)
+		for k = 1, total do
+			local dat = tContent[k]
+
+			local tx, ty = getCircleItemPos(x, y, r, total, k)
+
+			draw.Text(dat.text or "unknown", 8, tx, ty, dat.color or color_white, 1)
+		end
+
+		if total == 0 then
+			draw.Text("Empty", 10, x, y, color_white, 1, 1)
+		elseif v.text then
+			draw.Text(v.text, 6, x, y, color_white, 1, 1)
+		end
 	end
 end)
 
 hook.Add("zen.worldclick.nopanel.onPress", "zen.map_edit.quickmenu", function(code, tr)
-	if nvars.radial_menu.is_opened then
-		local selected = getSelected()
+	if code != MOUSE_LEFT then return end
+	if not nvars.radial_menu.is_opened then return end
 
-		if selected and selected > 0 and code == MOUSE_LEFT then
-			local tButton = nvars.mt_EntityButtons[selected]
+	for k, v in pairs(nvars.circles) do
+		local selID = v.SelectID
+		local selValue = v.SelectValue
 
-			nt.Send("nvars.run_command", {"entity", "int12", "next", "any"}, {founded_entity, tButton.id, tButton.mode != nil and true or false, tButton.mode})
+		if v.type == "nvars" then
+			local id, mode = selValue, v.value_mode
+
+			nt.Send("nvars.run_command", {"entity", "int12", "next", "any"}, {nvars.hoverEntity, id, mode != nil and true or false, mode})
 		end
-
-		nvars.radial_menu.Close()
 	end
+
+	nvars.radial_menu.Close()
 end)
 
 function nvars.radial_menu.Open()
 	nvars.radial_menu.is_opened = true
 	gui.EnableScreenClicker(true)
-	prevSelected = nil
 end
 
 function nvars.radial_menu.Close()
-	founded_entity = nil
-	nvars.mt_EntityButtons = {}
 	nvars.radial_menu.is_opened = false
-	r = 0
 	gui.EnableScreenClicker(false)
 end
 
 hook.Add("zen.map_edit.OnButtonPress", "quickmenu", function(ply, but, bind, vw)
-	if IsValid(vw.hoverEntity) then
-		if bind == IN_USE then
-			nvars.mt_EntityButtons = {}
-			nt.Send("nvars.get_buttons", {"entity"}, {vw.hoverEntity})
+	if bind != IN_USE then return end
+	if not IsValid(vw.hoverEntity) then return end
 
-            founded_entity = vw.hoverEntity
-            nvars.radial_menu.Open()
-            vw.IsRadialMenuOpen = true
-		end
-	end
+	nvars.circles.content = {}
+	nt.Send("nvars.get_buttons", {"entity"}, {vw.hoverEntity})
+
+	nvars.hoverEntity = vw.hoverEntity
+	nvars.radial_menu.Open()
 end)
 
 nt.Receive("nvars.get_buttons", {"entity", "table"}, function(ent, tButtons)
-    nvars.mt_EntityButtons = tButtons
+	nvars.circles[1].content = {}
+	local tContent = nvars.circles[1].content
+    for k, v in pairs(tButtons) do
+		table.insert(tContent, {
+			text = v.string,
+			value = v.id,
+			value_mode = v.mode,
+		})
+	end
 end)
 
 hook.Add("zen.map_edit.OnButtonUnPress", "quickmenu", function(ply, but, bind, vw)
-    if bind == IN_USE then
-        if nvars.radial_menu.is_opened then
-            nvars.radial_menu.Close()
-            vw.IsRadialMenuOpen = false
-        end
-    end
+    if bind != IN_USE then return end
+
+	if nvars.radial_menu.is_opened then
+		nvars.radial_menu.Close()
+	end
 end)
