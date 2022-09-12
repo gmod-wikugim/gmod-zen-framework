1819b0a11e7115e7b6914caa6c1359d33b7c864f
jtaylerj <jtaylerj@gmail.com>
2022-09-12 11:42:05 +0100

    moved to zen_sub

--- lua/zen/config.lua
+++ lua/zen/config.lua
@@ -1,3 +1,1 @@
 icfg.Admins = {}
-icfg.Admins["76561198272243731"] = true -- -243 King
-icfg.Admins["76561198302518525"] = true -- IceRP Code - Boris
--- lua/zen/integration/sv_white_list.lua
+++
@@ -1,3 +0,0 @@
-hook.Add("CheckPassword", "zen", function(sid64)
-    if iperm.PlayerSetPermission(sid64, "ignore_password") then return true end
-end)
