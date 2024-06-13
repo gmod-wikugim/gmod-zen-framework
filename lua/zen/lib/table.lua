44b2b1219c76f3c105879cb2cf1796f423ed31e1
jtaylerj <jtaylerj@gmail.com>
2024-06-13 13:27:55 +0000

    Add table.Get_WithoutDuplicatedValues, table.Get_WithoutValues

--- lua/zen/lib/table.lua
+++ lua/zen/lib/table.lua
@@ -22,3 +22,42 @@ function table.AddI(dest, source, index)
 
 	return dest
 end
+
+function table.Get_WithoutDuplicatedValues(source)
+    local result = {}
+
+    local m_Values = {}
+
+    for k, v in pairs(source) do
+        if m_Values[v] then continue end
+        m_Values[v] = true
+
+        table.insert(result, v)
+    end
+
+	return source
+end
+
+function table.Get_WithoutValues(dest, source)
+	if ( dest == source ) then return dest end
+
+	-- At least one of them needs to be a table or this whole thing will fall on its ass
+	if ( !istable( source ) ) then return dest end
+	if ( !istable( dest ) ) then dest = {} end
+
+    local t_Values = {}
+
+    for k, v in pairs(source) do
+        t_Values[v] = true
+    end
+
+	local result = {}
+
+    for k, v in pairs(dest) do
+        if t_Values[v] then continue end
+
+        table.insert(result, v)
+    end
+
+	return result
+end
