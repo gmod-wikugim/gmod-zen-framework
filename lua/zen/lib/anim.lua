b54f0d9755f6ccaf838ab53857b6b4c925b0bf5e
jtaylerj <jtaylerj@gmail.com>
2024-06-14 12:36:20 +0000

    Add anim.ResetGestureSlot, some fixes

--- lua/zen/lib/anim.lua
+++ lua/zen/lib/anim.lua
@@ -5,7 +5,7 @@ anim = _GET("anim")
 _LISTEN("nt.RegisterChannels", "anim", function()
 
     nt.RegisterChannel("anim.RestartGesture", nil, {
-        types = {"player", "uint32", "uint32", "boolean"},
+        types = {"player", "uint32", "uint32", "bool"},
         OnRead = function(self, _, ply, gusture_slot, act_number, auto_kill)
             if CLIENT then
                 anim.RestartGesture(ply, gusture_slot, act_number, auto_kill)
@@ -13,6 +13,15 @@ _LISTEN("nt.RegisterChannels", "anim", function()
         end,
     })
 
+
+    nt.RegisterChannel("anim.ResetGestureSlot", nil, {
+        types = {"player", "uint32"},
+        OnRead = function(self, _, ply, gusture_slot)
+            if CLIENT then
+                anim.ResetGestureSlot(ply, gusture_slot)
+            end
+        end,
+    })
 end)
 
 
@@ -22,10 +31,23 @@ end)
 ---@param act_number number
 ---@param auto_kill boolean?
 function anim.RestartGesture(ply, gusture_slot, act_number, auto_kill)
-    auto_kill = auto_kill or true
+
+    if auto_kill == nil then auto_kill = true end
     ply:AnimRestartGesture(gusture_slot, act_number, auto_kill)
 
     if SERVER then
-        nt.SendToChannel("anim.RestartGesture", nil, ply, gusture_slot, act_number, act_number)
+        nt.SendToChannel("anim.RestartGesture", nil, ply, gusture_slot, act_number, auto_kill)
+    end
+end
+
+
+---@param ply Player
+---@param gusture_slot number
+function anim.ResetGestureSlot(ply, gusture_slot)
+
+    ply:AnimResetGestureSlot(gusture_slot)
+
+    if SERVER then
+        nt.SendToChannel("anim.ResetGestureSlot", nil, ply, gusture_slot)
     end
 end
