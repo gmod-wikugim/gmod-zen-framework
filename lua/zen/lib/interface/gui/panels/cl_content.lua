031a322a21f94963013c54fcb208a15a7dc49242
vscode-srdev-pw <vscode@srdev.pw>
2024-06-27 15:28:18 +0000

    Fix content panel

--- lua/zen/lib/interface/gui/panels/cl_content.lua
+++ lua/zen/lib/interface/gui/panels/cl_content.lua
@@ -16,30 +16,8 @@ function PANEL:AutoSize(updateWide, updateTall)
     self.bChangeTall = updateTall
 end
 
-function PANEL:InstallOnChangeSizeHook(pnlChild)
-    pnlChild.OnSizeChanged = function()
-        _SizeToChildren(self, self.bChangeWide, self.bChangeTall)
-    end
-end
-
-function PANEL:RemoveOnChangeSizeHook(pnlChild)
-    pnlChild.OnSizeChanged = nil
-end
-
-function PANEL:OnChildAdded( child )
-    self:InstallOnChangeSizeHook(child)
-
-    if self.PostChildAdded then
-        self.PostChildAdded(child)
-    end
-end
-
-function PANEL:OnChildRemoved( child )
-    self:RemoveOnChangeSizeHook(child)
-
-    if self.PostChildRemoved then
-        self.PostChildRemoved(child)
-    end
+function PANEL:Think()
+    _SizeToChildren(self, self.bChangeWide, self.bChangeTall)
 end
 
 gui.RegisterStylePanel("content", PANEL, "EditablePanel")
