c3cb57f637c4b4d511f6206b3840567466da7e22
vscode_ws3.srdev.pw <wikugim@outlook.de>
2024-08-06 08:21:28 +0000

    Update lang

--- lua/zen/lib/language.lua
+++ lua/zen/lib/language.lua
@@ -5,25 +5,58 @@ module("zen", package.seeall)
 
     This module is responsible for managing the language system.
 
-    Easy Use:
-    - Insert `local L = language.L` at the top of the file
-    - Use L("your_phrase") to get translated phrase
-    - Use L("player_info", {1}) to get translated phrase with interpolation
-    - Use L("dmg_log", {attack = attacker, victim = victim}) to get translated phrase with interpolation
+    Links:
+        local P = language.GetLanguageForEdit("en")
+        local L = language.L
 
-    Easy Register:
-    - Get language table with `local P = language.GetLanguageForEdit("en")`
-    - Edit language table with `P["your_phrase"] = "Your phrase"`
-    - Use interpolated phrases with `P["player_info"] = "Player ${ply:1} has joined the server"`
- 
+    Examples:
+    - Just get translated phrase
+        P["welcome"] = "Welcome to Zen Framework!"
+        L("welcome") --> Welcome to Zen Framework!
+
+        P["physgun.name"] = "Physgun"
+        L("physgun.name") --> Physgun
+     - Simple interpolation
+        -- numeric index
+        P["entity_spawned"] = "Someone spawned $1"
+        L("entity_spawned", {ent}) --> Someone spawned Entity [0][worldspawn]
+
+        P["player_info"] = "Info: $1"
+        L("player_info", {LocalPlayer()}) --> Info: Player [1][Nick]
+        -- string index
+        P["entity_spawned"] = "Someone spawned $ent"
+        L("entity_spawned", {ent = ent}) --> Someone spawned Entity [0][worldspawn]
+
+        P["player_info"] = "Info: $ply"
+        L("player_info", {ply = LocalPlayer()}) --> Info: Player [1][Nick]
+    - Advanced interpolation
+        P["player_info"] = "Info: ${ply:1}"
+        L("player_info", {LocalPlayer()}) --> Info: Nick1
+
+        P["dmg_log"] = "Player ${ply:attacked} has damaged ${ply:victim} for ${n:damage} damage"
+        L("dmg_log", {attacked = LocalPlayer(), victim = LocalPlayer(), damage = 1111})
+            --> Player Nick1 has damaged Nick2 for 1.111 damage
+
+    Info:
+    - If language is not found, it will use default language
+    - if interpolated key is string and value not found, it will use key index(number) and try to find value with tab[index]
+        -- Register example:
+        P["player_info"] = "Player ${who} has joined the server"
+        P["player_info"] = "Player ${s:who} has joined the server"
+        -- Usage example:
+        L("player_info", {LocalPlayer()}) - Player Player Player [1][Nick] has joined the server
+        L("player_info", {who = LocalPlayer()}) - Player Player Player [1][Nick] has joined the server
+        -- You can use any combination of keys and indexes and it get same result
+
+
     Default interpolation types:
     - ply - Player Name as var:Nick()
     - n - Number with commas
     - s - String
     - arg - Argument as string
     - date|time - Date as os.date("!%H:%M %m.%d.%y", var), example: 12:00 01.01.21
- 
-    Usage: 
+
+    Usage:
     - language.GetLanguage() - Get current language
     - language.GetLanguagePhrase(lang_id, phrase) - Get phrase from language
     - language.GetTranslatedPhrase(phrase) - Get translated phrase
@@ -34,6 +67,7 @@ module("zen", package.seeall)
 language = _GET("language", language)
 
 language.mt_Langauges = language.mt_Langauges or {}
+local LL = language.mt_Langauges
 
 language.DEFAULT_LANG = "en"
 
@@ -55,32 +89,69 @@ local phrase_alias = {
 }
 
 local _concat = table.concat
-
+local _match = string.match
+local _gsub = string.gsub
+local _tostring = tostring
+local _tonumber = tonumber
 
 local function InterpolateConfig(message, read_types, phrase_alias, tab, onlyText)
     tab = tab or {}
-    message = message:gsub('($%b{})', function(w)
+
+    local iInterpolateCounter = 0
+    -- Simple interpolation
+    message = _gsub(message, "($[%w_-]+)", function(w)
+        iInterpolateCounter = iInterpolateCounter + 1
+
+        local value = w:sub(2)
+
+        if tab[value] then
+            return _tostring(tab[value])
+        else
+            local counter_value = tab[iInterpolateCounter]
+            if counter_value then
+                return _tostring(counter_value)
+            end
+        end
+    end)
+
+    -- Advanced interpolation
+    message = _gsub(message, '($%b{})', function(w)
+        iInterpolateCounter = iInterpolateCounter + 1
+
         local value = w:sub(3, -2)
 
         if phrase_alias[value] then
             return phrase_alias[value]()
         end
 
-        local key, n_value = string.match(value, "(%a+):(.+)")
+        local strict_type, tab_key = _match(value, "(%a+):(.+)")
 
-        do -- Check is number
-            local number_value = tonumber(n_value)
+        if strict_type and tab_key then
+            local isNumberic
+            local number_value = _tonumber(tab_key)
             if number_value then
-                n_value = number_value
+                tab_key = number_value
+                isNumberic = true
             end
+
+            local value = tab[tab_key]
+
+            if !value and !isNumberic then
+                value = tab[iInterpolateCounter]
+            end
+
+            if read_types[strict_type] then
+                value = read_types[strict_type](value, onlyText)
+            end
+
+            if !value then
+                return _concat{"${",strict_type, ':"', tostring(tab_key), '"}'}
+            end
+
+            return value
         end
 
-
-        if read_types[key] then
-            return read_types[key](tab[n_value], onlyText)
-        else
-            return (tab[n_value] and _concat{"${",key, ':"', tostring(tab[n_value]), '"}'} or w)
-        end
+        return w
     end)
 
     return message
@@ -110,16 +181,17 @@ end
 
 ---@param lang_id string
 ---@param phrase string
----@return string
+---@return string|nil
 function language.GetLanguagePhrase(lang_id, phrase)
     local lang_id = lang_id or language.DEFAULT_LANG
 
+    local translated = LL[lang_id] and LL[lang_id][phrase]
 
-    if !language.mt_Langauges[lang_id] then
-        return phrase
+    if !translated then
+        translated = LL[language.DEFAULT_LANG] and LL[language.DEFAULT_LANG][phrase]
     end
 
-    return language.mt_Langauges[lang_id][phrase] or phrase
+    return translated
 end
 
 -- PrintTable(language.mt_Langauges)
@@ -136,10 +208,8 @@ function language.GetTranslatedPhrase(phrase)
 
     local translated = language.GetLanguagePhrase(lang_id, phrase)
 
-    if translated == nil or translated == phrase then
-        translated = language.GetLanguagePhrase(language.DEFAULT_LANG, phrase)
-    else
-        return phrase
+    if !translated then
+        translated = phrase
     end
 
     return translated
@@ -157,3 +227,9 @@ function language.GetTranslatedPhraseInterpolate(phrase, tab, onlyText)
     return interpolated
 end
 language.L = language.GetTranslatedPhraseInterpolate
+
+
+if CLIENT then
+    local translated = language.L("welcome_player", {LocalPlayer()})
+    _print(translated)
+end
